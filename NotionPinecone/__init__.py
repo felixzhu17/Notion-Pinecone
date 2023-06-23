a3a32c30258b8f964d0ebfb5c0c445b6f8fafe08
felixzhu17 <80929644+felixzhu17@users.noreply.github.com>
2023-06-23 21:29:59 +1000

    Update __init__.py

--- NotionPinecone/__init__.py
+++ NotionPinecone/__init__.py
@@ -1,5 +1,5 @@
-from embedding import ADA_V2, Embedder
-from notion import NotionPinecone
-from pinecone import PineconeVectorStore
+from .embedding import ADA_V2, Embedder
+from .notion import NotionPinecone
+from .pinecone import PineconeVectorStore
 
 __version__ = "0.0.6"
