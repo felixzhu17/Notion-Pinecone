be207a78164e70df9ca0ac7e12ccdcb661057c58
felixzhu17 <zhu.felix@outlook.com>
2023-06-27 17:26:23 +1000

    Bump version: 0.0.19 → 0.0.20

--- NotionPinecone/__init__.py
+++ NotionPinecone/__init__.py
@@ -3,4 +3,4 @@ from .notion import NotionPinecone
 from .pinecone import PineconeVectorStore
 
 
-__version__ = "0.0.19"
+__version__ = "0.0.20"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="notion-pinecone",
-    version="0.0.19",
+    version="0.0.20",
     license="MIT",
     author="Felix Zhu",
     author_email="zhu.felix@outlook.com",
